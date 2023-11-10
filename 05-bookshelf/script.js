5ac8240bc1de0e56bc98b7cf17ecb3f4f5406b01
Ricardop970326 <ricardop.alejandrog@gmail.com>
2023-11-10 16:30:25 -0500

    update

---
+++ 05-bookshelf/script.js
@@ -0,0 +1,13 @@
+class bookshelf{
+    constructor(){
+        var Books = bookName;
+    }
+   
+    addBook(bookName){
+        Books.push(bookName);
+    }
+}
+
+var Bookshelf = new bookshelf();
+Bookshelf.addBook('the great gatsby');
+console.log(Bookshelf);
